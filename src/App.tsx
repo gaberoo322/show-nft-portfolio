c73c884d79e8b871c5b467b64848f3af4c650401
gaberoo322 <developergreenfield@gmail.com>
2022-12-05 19:35:06 -0700

    integrated ether.js and added connect logic to show nfts of connected wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,66 @@
 import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 import { getNFTs, NftData } from "./webservice";
 
 function App() {
-  const address = "greenfield.eth";
+  const provider = new ethers.providers.Web3Provider(window?.ethereum);
+  const [signer, setSigner] = useState<ethers.providers.JsonRpcSigner>(
+    provider.getSigner()
+  );
   const [nftData, setNftData] = useState<NftData>();
+  const [userAddress, setUserAddress] = useState<string>();
+
+  const initialize = async () => {
+    const signer = provider.getSigner();
+    await setSigner(signer);
+    const address = await signer.getAddress();
+    setNftData(await getNFTs(address));
+    setUserAddress(address);
+  };
 
   useEffect(() => {
-    const getNftData = async () => await getNFTs(address);
-    getNftData().then((response) => {
-      console.log("🚀 ~ file: App.tsx:15 ~ useEffect ~ response", response);
-      return setNftData(response);
-    });
+    if (window?.ethereum._state.accounts.length > 0) {
+      initialize();
+    }
   }, []);
 
+  const walletConnect = async () => {
+    await provider.send("eth_requestAccounts", []);
+    initialize();
+  };
+
   return (
-    <div className="flex-auto flex-col items-center bg-slate-800">
-      <h1 className="text-3xl text-white underline">Show those NiFTys</h1>
-      <div className="grid gap-5 grid-cols-4 grid-rows-none">
-        {nftData?.ownedNfts?.map((nft) => (
-          <div>
-            <div className="text-slate-400">{nft?.title}</div>
-            <img
-              className={"w-15"}
-              src={`${nft?.media?.[0]?.gateway}`}
-              alt={nft?.title}
-            />
-          </div>
-        ))}
-      </div>
+    <div className="flex-auto h-screen flex-col items-center px-10 py-8">
+      <header className="flex justify-between">
+        <div className="text-3xl text-white">Your NFT Portfolio</div>
+        <button
+          className="rounded-full bg-sky-500 hover:bg-sky-700 text-white p-2"
+          onClick={walletConnect}
+        >
+          {!userAddress
+            ? "Connect MetaMask Wallet"
+            : `Connected: ${userAddress}`}
+        </button>
+      </header>
+      {nftData ? (
+        <div className="grid gap-5 grid-cols-4 grid-rows-none pt-10">
+          {nftData?.ownedNfts?.map((nft) => (
+            <div>
+              <div className="text-slate-400">{nft?.title}</div>
+              <img
+                className={"w-15"}
+                src={`${nft?.media?.[0]?.gateway}`}
+                alt={nft?.title}
+              />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="py-10 mx-auto text-center text-white">
+          Connect your wallet to see your NFTs
+        </div>
+      )}
     </div>
   );
 }
